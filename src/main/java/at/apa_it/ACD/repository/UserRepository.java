4bc1954706ecdff39044c82162b03d74de3bf794
Avsar <Avsar@mmet>
2014-04-10 00:56:44 +0200

    -Rest modified -Added resources -Version conflicts solved -DispatcherServlet is being used

--- src/main/java/at/apa_it/ACD/repository/UserRepository.java
+++ src/main/java/at/apa_it/ACD/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package at.apa_it.ACD.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import at.apa_it.ACD.domain.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
 	public User findByUsername(String username);
+	@Override
+	public List<User> findAll();
 }
