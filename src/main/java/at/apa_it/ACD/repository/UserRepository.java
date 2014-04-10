192ee49b7755496ffee50033345bfb3609478ba0
Avsar <Avsar@mmet>
2014-04-10 14:42:23 +0200

    -Testconfiguration added

--- src/main/java/at/apa_it/ACD/repository/UserRepository.java
+++ src/main/java/at/apa_it/ACD/repository/UserRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.repository.CrudRepository;
 import at.apa_it.ACD.domain.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
-	public User findByUsername(String username);
+	public List<User> findByUsername(String username);
 	@Override
 	public List<User> findAll();
 }
--- src/test/java/at/apa_it/ACD/repository/WhenUsingUserRepository.java
+++ src/test/java/at/apa_it/ACD/repository/WhenUsingUserRepository.java
@@ -1,32 +1,37 @@
 package at.apa_it.ACD.repository;
 
-import org.junit.After;
-import static org.junit.Assert.*;
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.nullValue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
 import at.apa_it.ACD.domain.User;
 
-public class WhenUsingUserRepository extends AbstractJUnit4SpringContextTests{
+@ContextConfiguration(classes = RepositoryTestConfiguration.class)
+public class WhenUsingUserRepository extends AbstractJUnit4SpringContextTests {
 
-//	@Autowired
-//	UserRepository userRepository;
-//	
-//	@Before
-//    public void setup() {
-//        userRepository.deleteAll();
-//    }
-//
-//    @Test
-//    public void EnsureNotExistingUserCantBeFoundByUsernameCorrectly() throws Exception{
-//    	User user = userRepository.findByUsername("NullUser");
-//    	
-//    	assertThat(user, is(nullValue()));
-//    }
+    @Autowired
+    UserRepository userRepository;
+
+    @Before
+    public void setup() {
+        userRepository.deleteAll();
+
+        userRepository.save(new User("User1"));
+        userRepository.save(new User("User2"));
+        userRepository.save(new User("User3"));
+    }
+
+    @Test
+    public void EnsureUsersCanBeFoundByUsernameCorrectly() {
+        List<User> byName = userRepository.findByUsername("User1");
+
+        Assert.assertNotNull(byName);
+        Assert.assertEquals(1, byName.size());
+        Assert.assertNotNull(byName.get(0));
+    }
 }
